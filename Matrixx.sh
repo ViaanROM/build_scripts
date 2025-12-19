a5056a1cba4a33b68188fefce5da556de98ec9ae
ViaanROM <viaan1207@gmail.com>
2025-12-19 07:37:40 +0530

    Update Matrixx.sh

--- Matrixx.sh
+++ Matrixx.sh
@@ -3,21 +3,21 @@
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/oneplus && \
 rm -rf prebuilts/clang/host/linux-x86 \
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs -g default,-mips,-darwin,-notdefault && \
+repo init -u https://github.com/ProjectMatrixx/android.git -b 16.0 --git-lfs && \
 /opt/crave/resync.sh && \
-git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry && \
-git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common && \
-git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry && \
-git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common && \
-git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375 && \
-git clone https://github.com/anshedu/android_hardware_oplus -b lineage-22.2 hardware/oplus && \
+git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-23.0 device/oneplus/larry && \
+git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-23.0 device/oneplus/sm6375-common && \
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-23.0 vendor/oneplus/larry && \
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-23.0 vendor/oneplus/sm6375-common && \
+git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-23.0 kernel/oneplus/sm6375 && \
+git clone https://github.com/anshedu/android_hardware_oplus -b lineage-23.0 hardware/oplus && \
 # Vanilla Build
 . build/envsetup.sh && \
-breakfast larry user && make installclean && mka bacon; \
+lunch lineage_larry-bp2a-user && make installclean && mka bacon; \
 rm -rf out/target/product/vanilla && rm -rf out/target/product/gapps; \
 cd out/target/product && mv larry vanilla && cd ../../..; \
 # Gapps Build
 cd device/oneplus/larry && rm lineage_larry.mk && mv gapps.txt lineage_larry.mk && cd ../../..; \
 . build/envsetup.sh; \
-breakfast larry user && make installclean && mka bacon; \
+lunch lineage_larry-bp2a-user && make installclean && mka bacon; \
 cd out/target/product && mv larry gapps && cd ../../..; \
