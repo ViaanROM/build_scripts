3a959bfbd5703efdf2e3e5f3f87af3ef01f8afe5
ViaanROM <viaan1207@gmail.com>
2026-01-01 07:54:48 +0530

    Update EvolutionX.sh

--- EvolutionX.sh
+++ EvolutionX.sh
@@ -1,16 +1,12 @@
-#! /bin/bash
 
-rm -rf .repo/local_manifests; \
-rm -rf {device,vendor,kernel,hardware}/oneplus && \
-rm -rf prebuilts/clang/host/linux-x86 && \
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b bka --git-lfs -g default,-mips,-darwin,-notdefault && \
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
-git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry && \
-git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common && \
-git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry && \
-git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common && \
-git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375 && \
-git clone https://github.com/anshedu/android_hardware_oplus -b lineage-22.2 hardware/oplus && \
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs && \
+/opt/crave/resynx.sh && \
+git clone https://github.com/ViaanLarryROMS/android_device_oneplus_larry -b lineage-23.1 device/oneplus/larry && \
+git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-23.1 device/oneplus/sm6375-common && \
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-23.1 vendor/oneplus/larry && \
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-23.1 vendor/oneplus/sm6375-common && \
+git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-23.1 kernel/oneplus/sm6375 && \
+git clone https://github.com/anshedu/android_hardware_oplus -b lineage-23.1 hardware/oplus && \
 # Vanilla Build
 . build/envsetup.sh && \
 lunch lineage_larry-bp2a-user && make installclean && m evolution; \
@@ -20,4 +16,4 @@ cd out/target/product && mv larry vanilla && cd ../../..; \
 cd device/oneplus/larry && rm larry.mk && mv gapps.txt larry.mk && cd ../../..; \
 . build/envsetup.sh; \
 lunch lineage_larry-bp2a-user && make installclean && m evolution; \
-cd out/target/product && mv larry gapps && cd ../../..; \
+cd out/target/product && mv larry g
